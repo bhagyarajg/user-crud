546add4245772475cd707bf6ff001c2a221626c0
bhagyarajg <bhagyaraj.gaddam@gmail.com>
2018-01-07 00:24:30 -0500

    feat: initial app configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,31 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UserListComponentComponent } from './user-list-component/user-list-component.component';
+import { UserService } from './user-service/user.service';
+import { HttpModule } from '@angular/http';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'users', component: UserListComponentComponent },
+  { path: 'users/:id', component: UserDetailsComponent },
+  { path: '', redirectTo: '/users', pathMatch: 'full' }
+ /* { path: '**', component: PageNotFoundComponent }*/
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponentComponent,
+    UserDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
